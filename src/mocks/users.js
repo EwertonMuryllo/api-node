8285d197e7e914bbbd8496eda4e7ad3c95268612
ewertonmuryllo@gmail.com <ewertonsilva0977@digitalcollege.internal>
2026-01-31 10:46:18 -0300

    feat: create a new file to users data mocks

---
+++ src/mocks/users.js
@@ -0,0 +1,23 @@
+const users = [{
+  id: 1,
+  name: 'Ewerton Muryllo',
+  phone: '8552942525',
+  adress: {
+    street: 'Raimundo Alves Cavalcante',
+    number: 71
+  }  
+    ,id: 2,
+  name: 'Darius Flinty',
+  phone: '85524245',
+  adress: {
+    street: 'Raimundo Alves Cavalcante',
+    number: 71
+  }
+      ,id: 3,
+  name: 'Yume Tanaka',
+  phone: '8529443525',
+  adress: {
+    street: 'Raimundo Alves Cavalcante',
+    number: 71
+  }
+}]
