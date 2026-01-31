769d3c638d3e88e10fd428a7a2349d1e370d9abe
ewertonmuryllo@gmail.com <ewertonsilva0977@digitalcollege.internal>
2026-01-31 10:04:42 -0300

    chore: add header to response

--- src/server.js
+++ src/server.js
@@ -1,16 +1,30 @@
 import http from 'node:http';
 
 const user = {
-    id: crypto.randomUUID(),
-    name: 'Ewerton Muryllo',
-    adress: {
-        street: 'raimundo'
-    }
-
+  id: 1,
+  name: 'Ewerton Muryllo',
+  phone: '8552942525',
+  adress: {
+    street: 'Raimundo Alves Cavalcante',
+    number: 71
+  }
 }
 
+http.createServer(({ url, method }, response) => {
 
-http.createServer((request, response) => {
-    response.writeHead(200, {'content-type': 'text/html;charset=utf-8'})
-   response.end('hello Client!')
+  if (url === '/users' && method === 'GET') {
+    response.writeHead(
+      200,
+      { 'content-type': 'application/json; charset=utf-8' }
+    );
+    response.end(JSON.stringify(user));
+    return;
+  }
+
+  response.writeHead(
+    404,
+    { 'content-type': 'text/html; charset=utf-8' }
+  );
+  response.end(`Cannot ${method} ${url}`);
+
 }).listen(3000);
