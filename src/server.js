799c087f51d2fbe8d2c8d7047a74897e21134662
ewertonmuryllo@gmail.com <ewertonsilva0977@digitalcollege.internal>
2026-01-31 09:31:02 -0300

    config: add config to import/export

--- src/server.js
+++ src/server.js
@@ -1,5 +1,16 @@
 import http from 'node:http';
 
+const user = {
+    id: crypto.randomUUID(),
+    name: 'Ewerton Muryllo',
+    adress: {
+        street: 'raimundo'
+    }
+
+}
+
+
 http.createServer((request, response) => {
+    response.writeHead(200, {'content-type': 'text/html;charset=utf-8'})
    response.end('hello Client!')
 }).listen(3000);
